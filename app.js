32bb659e27af0fb7ac4195de3e7c198f805d1095
Jair Luna Alcantara <mrjairtorta@gmail.com>
2025-08-15 09:48:28 -0600

    añadiendo readme

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let inputAmigos = document.getElementById("amigo");
+let ulListaAmigos = document.getElementById("listaAmigos");
+let ulresultado = document.getElementById("resultado");
+//funcion para agregar amigos 
+function agregarAmigo() {
+    if (inputAmigos.value == "") {
+        alert("Ingresa un nombre");
+    }else{
+    amigos.push(inputAmigos.value);
+    ulListaAmigos.innerHTML += `<li>${inputAmigos.value}</li>`;
+    }
+}
+
+//funcion para sortear amigo
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("introdusca al menos 2 nombres");
+    }else{
+    let sorteoEntreAmigos = Math.floor(Math.random()*amigos.length);
+    let amigoSecreto = amigos[sorteoEntreAmigos];
+    ulresultado.innerHTML += `<li>el amigo secreto es:${amigoSecreto}</li>`;
+    }
+}
